47a164b0e82214d0a3f8d43c92b29ec212e9358e
Frederik Schorr <frederik.schorr@gmail.com>
2018-06-20 19:26:37 +0200

    working

--- predict_mobile_lstm.py
+++ predict_mobile_lstm.py
@@ -74,8 +74,11 @@ def main():
     sOflowFeatureDir = "data-temp/01-ledasila/%03d/oflow-mobilenet/val"%(nClasses)
 
     sModelDir        = "model"
-    sModelImage      = sModelDir + "/20180620-1653-ledasila021-image-mobile-lstm-best.h5"
-    sModelOflow      = sModelDir + "/20180620-1701-ledasila021-oflow-mobile-lstm-best.h5"
+    sModelImageBest  = sModelDir + "/20180620-1653-ledasila021-image-mobile-lstm-best.h5"
+    sModelImageLast  = sModelDir + "/20180620-1653-ledasila021-image-mobile-lstm-last.h5"
+    sModelOflowBest  = sModelDir + "/20180620-1701-ledasila021-oflow-mobile-lstm-best.h5"
+    sModelOflowLast  = sModelDir + "/20180620-1701-ledasila021-oflow-mobile-lstm-last.h5"
+
 
     print("\nPredict with combined rgb + flow models ... ")
     print(os.getcwd())
@@ -83,25 +86,31 @@ def main():
     # initialize
     oClasses = VideoClasses(sClassFile)
 
-    # predict on rgb features
-    fAcc_rgb, _, arProba_rgb, liLabels_rgb = predict(sImageFeatureDir, sModelImage, oClasses)
-    print("Image model accuracy: %.2f%%"%(fAcc_rgb*100.))
+    # predict on image features
+    fAcc_image_best, _, arProba_image_best, liLabels_image_best = predict(sImageFeatureDir, sModelImageBest, oClasses)
+    print("Image best model accuracy: %.2f%%"%(fAcc_image_best*100.))
+
+    fAcc_image_last, _, arProba_image_last, liLabels_image_last = predict(sImageFeatureDir, sModelImageLast, oClasses)
+    print("Image last model accuracy: %.2f%%"%(fAcc_image_last*100.))
 
     # predict on flow features
-    fAcc_flow, _, arProba_flow, liLabels_flow = predict(sOflowFeatureDir, sModelOflow, oClasses)
-    print("Optical flow model accuracy: %.2f%%"%(fAcc_flow*100.))
+    fAcc_oflow_best, _, arProba_oflow_best, liLabels_oflow_best = predict(sOflowFeatureDir, sModelOflowBest, oClasses)
+    print("Optical flow best model accuracy: %.2f%%"%(fAcc_oflow_best*100.))
+
+    fAcc_oflow_last, _, arProba_oflow_last, liLabels_oflow_last = predict(sOflowFeatureDir, sModelOflowLast, oClasses)
+    print("Optical flow last model accuracy: %.2f%%"%(fAcc_oflow_last*100.))
 
     # Combine predicted probabilities, first check if labels are equal
     if liLabels_rgb != liLabels_flow:
         raise ValueError("The rgb and flwo labels do not match")
 
-    arSoftmax = arProba_rgb + arProba_flow # shape = (nSamples, nClasses)
+    arSoftmax = arProba_image_best + arProba_image_last + arProba_oflow_best + arProba_oflow_last # shape = (nSamples, nClasses)
     arProba = np.exp(arSoftmax) / np.sum(np.exp(arSoftmax), axis=1).reshape(-1,1)
-    #print("arProba shape: %s. Sum of 3rd sample: %f (should be 1.0)"%(str(arProba.shape), np.sum(arProba[3,...])))
+    print("arProba shape: %s. Sum of 3rd sample: %f (should be 1.0)"%(str(arProba.shape), np.sum(arProba[3,...])))
 
     arPred = arProba.argmax(axis=1)
     fAcc = np.mean(liLabels_rgb == oClasses.dfClass.loc[arPred, "sClass"])
-    print("\nCombined RGB + flow model accuracy: %.2f%%"%(fAcc*100.))
+    print("\nCombined 4-model accuracy: %.2f%%"%(fAcc*100.))
 
     return
 
